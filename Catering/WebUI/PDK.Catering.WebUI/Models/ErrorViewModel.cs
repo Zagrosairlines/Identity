6896ff507d8f991821b3edcf7caaa9eded8d5c79
hoseini.h <hoseini.h@AHMADVAND>
2019-06-09 09:40:19 +0430

    initial

---
+++ Catering/WebUI/PDK.Catering.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDK.Catering.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
